5a979d8e7532762f65771bc8eba17184be355c63
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-18 13:11:18 +0530

    Optimistic vs Pessimistic Updates

--- src/App.js
+++ src/App.js
@@ -33,9 +33,21 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-   await Axios.delete(apiEndpoint + '/' + post.id);
-   const posts = this.state.posts.filter(p => p.id !== post.id );
-   this.setState({posts});
+
+    const originalPosts = this.state.posts;
+
+    const posts = this.state.posts.filter(p => p.id !== post.id );
+    this.setState({posts});
+
+    try{
+      await Axios.delete(apiEndpoint + '/' + post.id);
+      throw new Error('');
+    }
+    catch(err){
+      alert('Something wrong while deleting post');
+      this.setState({posts:originalPosts});
+    }
+   
   };
 
   render() {
